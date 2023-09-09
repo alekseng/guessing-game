9f057d79854e7c869cb26ea88e016a5203f9cfd1
alekseng <alexvdche@mail.ru>
2023-09-09 19:31:54 +0300

    feat: complite task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
     constructor() {}
 
-    setRange(min, max) {
+  minNum;
+  maxNum;
+  guessNum;
 
+    setRange(min, max) {
+      this.minNum = min;
+      this.maxNum = max;
     }
 
     guess() {
-
+      this.guessNum = Math.round((this.minNum + this.maxNum) / 2);
+      return this.guessNum;
     }
 
     lower() {
-
+      this.maxNum = this.guessNum;
     }
 
     greater() {
-
+      this.minNum = this.guessNum;
     }
 }
 
